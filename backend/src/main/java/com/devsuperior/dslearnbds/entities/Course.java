5308a85673a8836df87eee1d39ccb174bdb930e9
vitorlazarotto <vitor.lazarotto@equiplano.com.br>
2021-10-15 11:24:40 -0300

    Offer and Course, ManyToOne, OneToMany

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Course.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -21,7 +24,10 @@ public class Course implements Serializable {
 	private String name;
 	private String imgUri;
 	private String imgGrayUri;
-
+	
+	@OneToMany(mappedBy = "course") //nome do atributo e não da coluna
+	private List<Offer> offers = new ArrayList<>(); 
+	
 	public Course() {}
 
 	public Course(Long id, String name, String imgUri, String imgGrayUri) {
@@ -62,6 +68,10 @@ public class Course implements Serializable {
 
 	public void setImgGrayUri(String imgGrayUri) {
 		this.imgGrayUri = imgGrayUri;
+	}
+	
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
 	@Override
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp JavaScript', 'https://res.cloudinary.com/dte7upwcr/image/upload/f_auto,w_1500/v1618959177/blog/blog2/curso-de-programacao/curso-de-programacao-img_header.jpg', 'https://www2.educacao.mg.gov.br/images/stories/noticias/2021/08-Agosto/CURSOSESCOLADEFORMAO.jpg');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2022-01-10T20:00:00Z', TIMESTAMP WITH TIME ZONE '2022-05-01T20:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-06-10T20:00:00Z', TIMESTAMP WITH TIME ZONE '2022-12-01T20:00:00Z', 1);
