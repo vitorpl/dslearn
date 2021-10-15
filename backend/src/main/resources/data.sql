9c8bab3e42317b3bac47cbe91bfe134ed93eef5d
vitorlazarotto <vitor.lazarotto@equiplano.com.br>
2021-10-15 12:17:53 -0300

    Resources

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,3 +17,12 @@ INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp JavaScript
 
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2022-01-10T20:00:00Z', TIMESTAMP WITH TIME ZONE '2022-05-01T20:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-06-10T20:00:00Z', TIMESTAMP WITH TIME ZONE '2022-12-01T20:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) values
+('Trilha JS', 'Trilha principal do curso', 1, 'https://res.cloudinary.com/dte7upwcr/image/upload/f_auto,w_1500/v1618959177/blog/blog2/curso-de-programacao/curso-de-programacao-img_header.jpg', 1, 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) values
+('Fórum', 'Tire suas dúvidas', 2, 'https://res.cloudinary.com/dte7upwcr/image/upload/f_auto,w_1500/v1618959177/blog/blog2/curso-de-programacao/curso-de-programacao-img_header.jpg', 2, 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) values
+('Lives', 'Lives ao vivo exclusivas', 3, 'https://res.cloudinary.com/dte7upwcr/image/upload/f_auto,w_1500/v1618959177/blog/blog2/curso-de-programacao/curso-de-programacao-img_header.jpg', 0, 1);
