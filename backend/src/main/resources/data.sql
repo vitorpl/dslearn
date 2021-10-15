6dc15d71751d5cb73f2f50e3860dd67fdc412d2e
vitorlazarotto <vitor.lazarotto@equiplano.com.br>
2021-10-15 10:54:14 -0300

    Course Entity

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp JavaScript', 'https://res.cloudinary.com/dte7upwcr/image/upload/f_auto,w_1500/v1618959177/blog/blog2/curso-de-programacao/curso-de-programacao-img_header.jpg', 'https://www2.educacao.mg.gov.br/images/stories/noticias/2021/08-Agosto/CURSOSESCOLADEFORMAO.jpg');
