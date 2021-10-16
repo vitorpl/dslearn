e37e2f632016bfe4b381bcb15908ff70735dd892
vitorlazarotto <vitor.lazarotto@equiplano.com.br>
2021-10-16 15:40:25 -0300

    Content, Lesson, Task - MApeamento com herança

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -0,0 +1,125 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+//a classe abstract pois não é criada uma instância direta de Lesson, apenas Task ou Content
+@Entity
+@Table(name = "tb_lesson")
+/** 
+ * A estratégia pode ser "Tabelão", ou seja, com colunas da subclasse não utilizada nulos
+ * Também pode ser com duas tabelas, uma para cada subclasse (Joined)
+ */
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Lesson implements Serializable {
+
+	private static final long serialVersionUID = 4860842036007944624L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Integer Position; 
+	
+	@ManyToOne
+	@JoinColumn(name = "section_id")
+	private Section section;
+	
+	//não colocar coleções no construtor, também evitar colocar o método set
+	//associação muitos para muitos!
+	@ManyToMany 
+	@JoinTable(
+			name = "tb_lessons_done",
+			joinColumns = @JoinColumn(name = "lesson_id"), //onde estou
+			inverseJoinColumns = { //chave de enrollment é composta, então muda um pouco
+					@JoinColumn(name = "user_id"),
+					@JoinColumn(name = "offer_id")
+					} 
+			)
+	private Set<Enrollment> enrollmensDone = new HashSet<>();
+	
+	public Lesson() {}
+
+	public Lesson(Long id, String title, Integer position, Section section) {
+		super();
+		this.id = id;
+		this.title = title;
+		Position = position;
+		this.section = section;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getPosition() {
+		return Position;
+	}
+
+	public void setPosition(Integer position) {
+		Position = position;
+	}
+
+	public Section getSection() {
+		return section;
+	}
+
+	public void setSection(Section section) {
+		this.section = section;
+	}
+
+	public Set<Enrollment> getEnrollmensDone() {
+		return enrollmensDone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lesson other = (Lesson) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
