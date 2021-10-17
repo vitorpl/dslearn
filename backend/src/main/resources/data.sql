268d147311c6d472fc592e23f31287a2ccbcd66b
vitorlazarotto <vitor.lazarotto@equiplano.com.br>
2021-10-17 15:35:47 -0300

    Preauthorizing methods!

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -6,7 +6,7 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
-INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
